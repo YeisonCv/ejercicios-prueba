7ceac51202125a9074eec778f154ccf3e51a0d45
YeisonCv <yeisoncv10v@gmail.com>
2026-09-11 19:36:18 -0500

    fix: add icov to vitest.config reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ test: {
     include: ["test/**/*.spec.ts"],
     coverage: {
         provider: "v8",
-        reporter: ["text", "html"],
+        reporter: ["text", "html", "icov"],
         include: ["src/**/*.ts"],
         exclude: [
         ...defaultExclude,
